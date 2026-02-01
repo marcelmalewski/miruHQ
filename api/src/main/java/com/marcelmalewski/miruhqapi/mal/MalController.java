5d5688c49af6ee8d055539afbfe9e423515bb1db
Marcel Malewski <mmalewski9@gmail.com>
2026-02-01 21:47:12 +0100

    Update

--- api/src/main/java/com/marcelmalewski/miruhqapi/mal/MalController.java
+++ api/src/main/java/com/marcelmalewski/miruhqapi/mal/MalController.java
@@ -1,12 +1,33 @@
 package com.marcelmalewski.miruhqapi.mal;
 
+
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MalController {
+
+    private String currentState;
+    private String redirectUri = "http://localhost:8080/oauth/mal/callback";
+
+    @Value("${mal.client-id}")
+    private String clientId;
+    @Value("${mal.client-secret}")
+    private String clientSecret = "YOUR_CLIENT_SECRET";
+
     private final MalService malService;
 
     public MalController(MalService malService) {
@@ -16,5 +37,35 @@ public class MalController {
     @PostMapping("/api/anime/search")
     public List<AnimeDto> searchAnime(@RequestBody AnimeSearchRequest request) {
         return malService.search(request);
+    }
+
+    @PostMapping("/api/authenticate")
+    public void authenticate(HttpServletResponse response) throws IOException {
+        currentState = UUID.randomUUID().toString();
+
+        String authorizeUrl =
+            "https://myanimelist.net/v1/oauth2/authorize" +
+                "?response_type=code" +
+                "&client_id=" + clientId +
+                "&redirect_uri=" + URLEncoder.encode(redirectUri, StandardCharsets.UTF_8) +
+                "&scope=write:users" +
+                "&state=" + currentState +
+                "&code_challenge=" + currentState +
+                "&code_challenge_method=plain";
+
+        response.sendRedirect(authorizeUrl);
+    }
+
+    @GetMapping("/oauth/mal/callback")
+    public ResponseEntity<String> callback(String code, String state) {
+
+        if (!Objects.equals(state, currentState)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body("Invalid state!");
+        }
+
+        return ResponseEntity.ok(
+            "OAuth successful Authorization code: " + code
+        );
     }
 }
