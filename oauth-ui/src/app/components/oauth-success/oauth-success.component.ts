99e39e59cd16769c5059d9e08dcf7d082439823f
Marcel Malewski <mmalewski9@gmail.com>
2026-03-28 21:53:21 +0100

    Update

--- oauth-ui/src/app/components/oauth-success/oauth-success.component.ts
+++ oauth-ui/src/app/components/oauth-success/oauth-success.component.ts
@@ -1,7 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ouath-success',
   templateUrl: './oauth-success.component.html',
 })
-export class OauthSuccessComponent {}
+export class OauthSuccessComponent implements OnInit {
+  ngOnInit() {
+    const hash = new URLSearchParams(window.location.hash.substring(1));
+    const code = hash.get('code');
+    const state = hash.get('state');
+    history.replaceState(null, '', window.location.pathname);
+
+    if (!code || !state) {
+      console.error('Missing code/state');
+      return;
+    }
+
+    fetch('http://localhost:8080/api/oauth/mal/exchange', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        code,
+        state,
+      }),
+      credentials: 'include',
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Exchange failed');
+        }
+        return res.json();
+      })
+      .then((token) => {
+        console.log('TOKEN RECEIVED', token);
+        window.postMessage(
+          {
+            type: 'MAL_OAUTH_SUCCESS',
+            payload: token,
+          },
+          '*',
+        );
+      })
+      .catch((err) => {
+        console.error('OAuth exchange failed', err);
+      });
+  }
+}
