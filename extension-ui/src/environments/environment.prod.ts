5f9c7bf30209b67cf7824612c3264e87f71225a1
Marcel Malewski <mmalewski9@gmail.com>
2026-06-12 10:40:16 +0200

    Update

--- extension-ui/src/environments/environment.prod.ts
+++ extension-ui/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   backendUrl: 'https://miruhq-api.onrender.com',
+  oauthUrl: 'https://www.miruhq.org',
 };
--- extension-ui/src/environments/environment.ts
+++ extension-ui/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   backendUrl: 'http://localhost:8080',
+  oauthUrl: 'http://localhost:4200',
 };
